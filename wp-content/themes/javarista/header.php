fee01a4f630bd0b863e9dc72dbff638d427d86ae
Alyson Burton <funkyfrog3000@Alyson-Burtons-MacBook-Pro.local>
2016-04-19 20:35:19 -0700

    styling for home

--- wp-content/themes/javarista/header.php
+++ wp-content/themes/javarista/header.php
@@ -7,6 +7,10 @@
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+        <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/bootstrap.min.css">
+         <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/bootstrap-theme.min.css">
+         <link rel="stylesheet" href="<?php bloginfo( 'template_directory' ); ?>/css/style.css">
+         <link href='https://fonts.googleapis.com/css?family=Roboto:400,100' rel='stylesheet' type='text/css'>
         <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>
         <!-- add in roboto -->
         <?php wp_head(); ?>
